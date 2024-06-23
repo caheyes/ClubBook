7776da58e8eeb7a1eb4b9d6d5515b9deffea65aa
Caroline Souza <carol.silvasouza@hotmail.com>
2024-06-23 20:52:17 -0300

    feat: Criando a tela de login

---
+++ src/app/assets/images/book-login/book-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'book-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-login.component.html',
+  styleUrl: './book-login.component.scss'
+})
+export class BookLoginComponent {
+
+}
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { BookLoginComponent } from '../../assets/images/book-login/book-login.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, FormGroupDirective, NgForm, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+};
+
+@Component({
+  selector: 'login',
+  standalone: true,
+  imports: [BookLoginComponent, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  emailForm = new FormControl('', [Validators.required, Validators.email]);
+  senhaForm = new FormControl('', [Validators.required]);
+
+  matcher = new MyErrorStateMatcher();
+}
