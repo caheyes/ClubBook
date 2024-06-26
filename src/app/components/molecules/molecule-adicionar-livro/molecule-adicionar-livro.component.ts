797ea5842b7ad7c60bdbbeaa81c3d9948cb735a4
Caroline Souza <carol.silvasouza@hotmail.com>
2024-06-26 01:22:22 -0300

    feat: Foi feito o post

--- src/app/components/molecules/molecule-adicionar-livro/molecule-adicionar-livro.component.ts
+++ src/app/components/molecules/molecule-adicionar-livro/molecule-adicionar-livro.component.ts
@@ -3,6 +3,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormControl, FormGroupDirective, NgForm, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ErrorStateMatcher} from '@angular/material/core';
+import { LivrosService } from '../../../services/livros.service';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -22,4 +23,22 @@ export class MoleculeAdicionarLivroComponent {
   nomeForm = new FormControl('', [Validators.required]);
   descricaoForm = new FormControl('', [Validators.required]);
   matcher = new MyErrorStateMatcher();
+
+  constructor(private livrosService: LivrosService) {}
+
+  //methods
+  postLivro() {
+    let data = {
+      nome: this.nomeForm.value,
+      descricao: this.descricaoForm.value,
+      usuario_id: "front@book.com",
+      estrelas: 4
+    };
+
+    console.log(data);
+
+    this.livrosService.postLivro(data).subscribe((livro: any) => {
+      console.log('deu bom');
+    });
+  }
 }
